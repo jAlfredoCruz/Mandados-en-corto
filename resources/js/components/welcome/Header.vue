a7485e488cc07fcc856917bbfc0017d225e06e07
J.Alfredo <alfredoinver@gmail.com>
2025-03-20 19:46:52 -0600

    Add initial project files including configuration, components, and tests

---
+++ resources/js/components/welcome/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+    <nav class="bg-gray-800 text-white py-3 px-4 flex items-center justify-between fixed top-0 w-full z-20">
+        <a class="font-bold text-xl tracking-tight" href="#">My Site</a>
+        <div class="flex items-center">
+            <a class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Home</a>
+            <a class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Sobre</a>
+            <a class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Contacto</a>
+            <a class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Quejas y sugerencias</a>
+        </div>
+    </nav>
+</template>
+
+
---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import Header from '../components/welcome/Header.vue';
+import Hero from '../components/welcome/Hero.vue';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div class="flex flex-col h-screen">
+        <Header />
+        <Hero class="flex-grow"/>
+    </div>
+</template>
