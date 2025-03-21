ae62ef3444a5cc311a8b2bd79185a17ec779191b
J.Alfredo <alfredoinver@gmail.com>
2025-03-20 19:54:23 -0600

    Update header title to 'Mandandos en corto'

--- resources/js/components/welcome/Header.vue
+++ resources/js/components/welcome/Header.vue
@@ -2,7 +2,7 @@
 
 <template>
     <nav class="bg-gray-800 text-white py-3 px-4 flex items-center justify-between fixed top-0 w-full z-20">
-        <a class="font-bold text-xl tracking-tight" href="#">My Site</a>
+        <a class="font-bold text-xl tracking-tight" href="#">Mandandos en corto</a>
         <div class="flex items-center">
             <a class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Home</a>
             <a class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" href="#">Sobre</a>
