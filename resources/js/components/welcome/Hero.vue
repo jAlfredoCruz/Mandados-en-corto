4109c7090184410668199ce04b15e7c6991129e9
J.Alfredo <alfredoinver@gmail.com>
2025-03-20 20:27:23 -0600

    Update hero image path and phone number in Hero component

--- resources/js/components/welcome/Hero.vue
+++ resources/js/components/welcome/Hero.vue
@@ -9,19 +9,19 @@
 <template>
    <div class="relative bg-gradient-to-r from-purple-600 to-blue-600 h-screen text-black overflow-hidden">
   <div class="absolute inset-0">
-    <img src="images/corto-hero.jpg" alt="Background Image" class="object-cover object-center w-full h-full" />
+    <img src="/images/corto-hero.jpeg" alt="Background Image" class="object-cover object-center w-full h-full" />
     <div class="absolute inset-0 bg-black opacity-50"></div>
   </div>
 
   <div class="relative z-10 flex flex-col justify-center items-center h-full text-center">
-    <h1 class="text-5xl font-bold leading-tight mb-4">¿Donde quieres recibir tu compra?</h1>
+    <h1 class="text-5xl font-bold leading-tight mb-4 text-black">¿Donde quieres recibir tu compra?</h1>
     <IconField class="bg-white ">
         <InputIcon class="">
             <Location />
         </InputIcon>
         <InputText  placeholder="Search" />
     </IconField>
-    <p class="text-4xl font-bold text-white mb-8 flex items-center justify-between"><span class="m-3">O haz el pedido por Whatsapp</span> <Whatsapp /> <span>4871450515</span> </p>
+    <p class="text-4xl font-bold text-white mb-8 flex items-center justify-between"><span class="m-3">O haz el pedido por Whatsapp</span> <Whatsapp /> <span>440 181 47 08</span> </p>
   </div>
 </div>
 
